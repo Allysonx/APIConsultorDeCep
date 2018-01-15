abbf95e3231bc4ec74bd97a1a2e3bd9ae9d52acc
Allysonx <ac.poli2016@gmail.com>
2018-01-15 17:03:46 -0200

    Modularizado Funcao para Obter Cep Formatado

--- app/Validator.js
+++ app/Validator.js
@@ -7,20 +7,19 @@ exports.validateCep = function(cep){
     let resultValidate;
 
     if(cep.length == 9 && cep.charAt(5) === '-'){
-    
-        cep = cep.substring(0, 5) + cep.substring(6, cep.length);
-
-        resultValidate = verifyCep(cep);
-
+        resultValidate = verifyCep(getFormattedCep(cep));
     } else if(cep.length == 8){
-
         resultValidate = verifyCep(cep);
     } else {
-
         resultValidate = false;
     }
 
     return resultValidate;
+}
+
+function getFormattedCep(cep){
+
+    return cep.substring(0, 5) + cep.substring(6, cep.length);
 }
 
 function isNumber(number){
